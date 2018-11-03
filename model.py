2e2cd40624b4e4301a0536b1b6009f94fe9b72a4
ChristophCan <scheuermann.chr@gmail.com>
2018-11-03 15:28:33 +0100

    Added Dropout and maxPooling

--- model.py
+++ model.py
@@ -22,51 +22,51 @@ for subFolder in drivingSubNames:
         for line in reader:
             line.append(subFolder)
             samples.append(line)
-            
+
 #Split the data in training and validation set
 from sklearn.model_selection import train_test_split
 train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
 
 def generator(samples, batch_size=32):
-    num_samples = len(samples)    
-    
+    num_samples = len(samples)
+
     while 1:
-        shuffle(samples)            
-        
+        shuffle(samples)
+
         #Processing the batches with given batch_size
         for offset in range(0, num_samples, batch_size):
-            batch_samples = samples[offset:offset+batch_size]         
-                        
+            batch_samples = samples[offset:offset+batch_size]
+
             images = []
             measurements = []
-            
+
             #Iterating through all training pictures
             for batch_sample in batch_samples:
-                
+
                 #Iterating through center, left and right image
                 for i in range(3):
-                    ImagePath, ImageName = split(batch_sample[i])               
-                    image = cv2.imread(join(drivingDataName, batch_sample[7], 'IMG', ImageName))                                       
-                                
+                    ImagePath, ImageName = split(batch_sample[i])
+                    image = cv2.imread(join(drivingDataName, batch_sample[7], 'IMG', ImageName))
+
                     #Adding image and flipped image
                     images.append(image)
                     images.append(cv2.flip(image,1))
-        
-                    #Read SteeringData for center, left and right image     
+
+                    #Read SteeringData for center, left and right image
                     if i==0:
                         measurement = float(batch_sample[3])
                     elif i==1:
                         measurement = float(batch_sample[3]) + 0.25
                     elif i==2:
-                        measurement = float(batch_sample[3]) - 0.25                         
-                
+                        measurement = float(batch_sample[3]) - 0.25
+
                     #Adding Steering data and flipped data
                     measurements.append(measurement)
                     measurements.append(measurement * -1.0)
-                    
+
             X_train = np.array(images)
-            y_train = np.array(measurements)                        
+            y_train = np.array(measurements)
             yield shuffle(X_train,y_train)
 
 from keras.backend import clear_session
@@ -105,12 +105,16 @@ model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3), output_sh
 model.add(Cropping2D(cropping=((70,25), (0,0))))
 model.add(Conv2D(24,(5,5),strides = (2,2),activation='relu'))
 model.add(Conv2D(36,(5,5),strides = (2,2),activation='relu'))
-model.add(Conv2D(48,(5,5),strides = (2,2),activation='relu'))
-model.add(Conv2D(64,(3,3),strides = (1,1),activation='relu'))
-model.add(Conv2D(64,(3,3),strides = (1,1),activation='relu'))
+model.add(Conv2D(48,(5,5),strides = (2,2)))
 
-#model.add(MaxPooling2D((2, 2)))
-model.add(Dropout(0.3))
+model.add(MaxPooling2D((2, 2)))
+model.add(Dropout(0.5))
+model.add(Activation('relu'))
+
+model.add(Conv2D(64,(2,2),strides = (1,1),activation='relu'))
+model.add(Conv2D(64,(1,1),strides = (1,1),activation='relu'))
+
+
 
 model.add(Flatten())
 model.add(Dense(100))
